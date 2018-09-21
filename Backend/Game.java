8885edc142125df64c000f4cd9e456102465d5f2
Sam Kravitz <samkravitz99@gmail.com>
2018-09-21 15:46:23 -0400

    Add preflop action for big blind

--- Backend/Game.java
+++ Backend/Game.java
@@ -61,8 +61,8 @@ public class Game {
 
         System.out.println(players[0].getName() + "'s action. Pot = " + pot + " Stack = " + players[0].getStack());
 
-        decision:
-        while(true) {
+        sbDecision:
+        while (true) {
             System.out.println("Enter your decision: ");
             System.out.println("[c] - call (0.5)");
             System.out.println("[r] - raise");
@@ -70,10 +70,11 @@ public class Game {
             System.out.println("");
 
             String buttonDecision = s.nextLine();
+            s.nextLine();
             switch(buttonDecision) {
                 case "c": players[0].setStack(players[0].getStack() - 0.5);
                           pot += 0.5;
-                          break decision;
+                          break sbDecision;
                 case "r": raise:
                         while (true) {
                             System.out.println("Enter total raise size: ");
@@ -83,17 +84,57 @@ public class Game {
                                 continue raise;
                             } 
                             pot += raise - smallBlind;
-                            break decision;
+                            break sbDecision;
                         }
                 case "f": players[0].setInHand(false);
                         return;
                 default: System.out.println("Invalid option.");
+                         continue sbDecision;
+            }
+        }
+
+        System.out.println(players[1].getName() + "'s action. Pot = " + pot + " Stack = " + players[1].getStack());
+
+        bbDecision:
+        while (true) {
+            double callAmount = pot - 1.0;
+            System.out.println("Enter your decision: ");
+            if (pot == 2.0) {
+                System.out.println("[x] - check");
+                System.out.println("[r] - raise \n");
+            } else {
+                System.out.println("[c] - call (" + callAmount + ")");
+                System.out.println("[r] - raise");
+                System.out.println("[f] - fold \n");
+            }
+
+            String bbDecision = s.nextLine();
+            switch(bbDecision) {
+                case "x": return;
+                case "c": players[1].setStack(players[1].getStack() - callAmount);
+                          pot += callAmount;
+                          break bbDecision;
+                case "r": raise:
+                        while (true) {
+                            System.out.println("Enter total raise size: ");
+                            double raise = s.nextDouble();
+                            if (! (raise >= 2 * pot)) {
+                                System.out.println("Invalid raise size!");
+                                continue raise;
+                            } 
+                            pot += raise - bigBlind;
+                            break bbDecision;
+                        }
+                case "f": players[1].setInHand(false);
+                        return;
+                default: System.out.println("Invalid option.");
+                         continue bbDecision;
             }
         }
     }
 
     private void getFlopAction() {
-
+        System.out.println("The flop is " + deck.deal().toString() + deck.deal().toString() + deck.deal().toString());
     }
 
     private void moveButton() {
