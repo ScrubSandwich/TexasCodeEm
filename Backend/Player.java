a01ec527d808caf25d24ba17d727b9c9c51aee62
Sam Kravitz <samkravitz99@gmail.com>
2018-09-21 01:52:53 -0400

    Changed hole cards

--- Backend/Player.java
+++ Backend/Player.java
@@ -1,22 +1,27 @@
 public class Player {
 
     private String name;
-    private Card[] holeCards = new Card[2];
+    private Card card1;
+    private Card card2;
 
     public Player(String name) {
         this.name = name;
     }
 
     public void setCard(int i, Card card) {
-        if (i >= 1 && i <= 2) {
-            holeCards[i] = card;
+        if (i == 1) {
+            card1 = card;
+        } else if (i == 2) {
+            card2 = card;
         }
         return;
     }
 
     public Card getCard(int i) {
-        if (i >= 1 && i <= 2) {
-            return holeCards[i];
+        if (i == 1) {
+            return card1;
+        } else if (i == 2) {
+            return card2;
         }
     }
 
