bfa19062e3fcb8541047dd42e5e87c0d44c26123
jmieczni <jmieczni@purdue.edu>
2018-09-27 23:21:47 -0400

    Remove default stuff

--- Frontend/texas-code-em/src/App.js
+++ Frontend/texas-code-em/src/App.js
@@ -7,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Texas Code 'Em</h1>
+          <h1 className="title">Texas Code 'Em</h1>          
         </header>
       </div>
     );
