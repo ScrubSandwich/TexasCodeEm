b4c6038da6d6fa5a2f43a97986c82816b12b8e49
jmieczni <jmieczni@purdue.edu>
2018-09-28 02:43:56 -0400

    Working button fetch

--- Frontend/texas-code-em/src/ButtonPanel.js
+++ Frontend/texas-code-em/src/ButtonPanel.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import "./ButtonPanel.css"
+import axios from 'axios'
 
 class ButtonPanel extends Component {
+
+  handleClick = () => {
+    console.log("button clicked");
+    const url = "http://localhost:8080/hello";
+
+    axios.get(url)
+      .then(res => {
+        console.log("s");
+        console.log(res);
+      })
+  }
 
   render() {
     return (
       <div className="ButtonPanel">
-         <button>Check</button>
+         <button onClick={this.handleClick}>Check</button>
          <button>Raise</button>
          <button>Fold</button>
       </div>
