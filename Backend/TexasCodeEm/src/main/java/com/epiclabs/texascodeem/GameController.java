a285b29d4aa477fe08871487a9a5d73171f2411d
jmieczni <jmieczni@purdue.edu>
2018-10-15 00:18:59 -0400

    Minor changes

--- Backend/TexasCodeEm/src/main/java/com/epiclabs/texascodeem/GameController.java
+++ Backend/TexasCodeEm/src/main/java/com/epiclabs/texascodeem/GameController.java
@@ -2,9 +2,6 @@ package com.epiclabs.texascodeem;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
-
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,12 +13,10 @@ public class GameController {
 	
 	public Map<String, Object> generateUserID(@RequestParam Map<String, Object> body) {
 		Map<String, Object> response = new HashMap<String, Object>();
-		//String user = body.get("username").toString();
 		
 		String ID = Utility.makeID();
 		response.put("status", HttpStatus.OK);
 		response.put("userID", ID);
 		return response;
 	}
-	
 }
--- Backend/TexasCodeEm/src/main/java/com/epiclabs/texascodeem/api/Utility.java
+++ Backend/TexasCodeEm/src/main/java/com/epiclabs/texascodeem/api/Utility.java
@@ -1,14 +1,19 @@
 package com.epiclabs.texascodeem.api;
+
 import java.util.Random;
 
 public class Utility {
+
+	private static int ID_LENGTH = 10;
 	
 	public static String makeID() {
 		Random rand = new Random();
 		String ID = "";
+
 		for (int i = 0; i < 4; i++) {
-			ID += rand.nextInt(10);
+			ID += rand.nextInt(ID_LENGTH);
 		}
+
 		return ID;
 	}
 }
