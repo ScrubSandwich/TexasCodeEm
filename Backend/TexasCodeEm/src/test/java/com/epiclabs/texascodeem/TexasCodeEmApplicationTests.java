93a4ce435079e60843df172fa472f772becf63c2
jmieczni <jmieczni@purdue.edu>
2019-07-31 11:17:30 -0400

    Add generateUserId test

--- Backend/TexasCodeEm/src/test/java/com/epiclabs/texascodeem/TexasCodeEmApplicationTests.java
+++ Backend/TexasCodeEm/src/test/java/com/epiclabs/texascodeem/TexasCodeEmApplicationTests.java
@@ -1,16 +1,35 @@
 package com.epiclabs.texascodeem;
 
+import com.epiclabs.texascodeem.api.Values;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.omg.CORBA.Any;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TexasCodeEmApplicationTests {
 
+	@Autowired
+	TexasCodeEmApplication application;
+
 	@Test
-	public void contextLoads() {
+	public void generateUserIdTest() {
+		Map<String, Object> body = new HashMap<>();
+		body.put("username", "test");
+		
+		Map<String, Object> response = application.generateUserID(body);
+
+		assertEquals(HttpStatus.OK, response.get("status"));
+		assertEquals(Values.ID_LENGTH, response.get("userID").toString().length());
 	}
 
 }
