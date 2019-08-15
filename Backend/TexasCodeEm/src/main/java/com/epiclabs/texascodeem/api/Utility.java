37e9500801d5e6e25ed46c9f724482574f6beeb7
Jacob Miecznikowski <22201389+ScrubSandwich@users.noreply.github.com>
2019-08-15 16:04:54 -0400

    Tests (#17)
    * Add generateUserId test

--- Backend/TexasCodeEm/src/main/java/com/epiclabs/texascodeem/api/Utility.java
+++ Backend/TexasCodeEm/src/main/java/com/epiclabs/texascodeem/api/Utility.java
@@ -8,8 +8,8 @@ public class Utility {
 		Random rand = new Random();
 		String ID = "";
 
-		for (int i = 0; i < 4; i++) {
-			ID += rand.nextInt(Values.ID_LENGTH);
+		for (int i = 0; i < Values.ID_LENGTH; i++) {
+			ID += rand.nextInt(9);
 		}
 
 		return ID;
